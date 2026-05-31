baf76eadf75b33eb139971df0209b43ea6c7bb26
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-05-31 18:53:34 +0200

    refactor(coordinator-service): introduce machine constants for limits and test values

---
+++ microservices/coordinator-service/test/constants/machine-values.ts
@@ -0,0 +1,17 @@
+export const MACHINE_VALUES = {
+  TEMPERATURE: {
+    SAFE: 50,
+    OVER: 90,
+    UNDER: 5
+  },
+
+  POWER_CONSUMPTION: {
+    SAFE: 40,
+    OVER: 120
+  },
+
+  EMISSION: {
+    SAFE: 30,
+    OVER: 100
+  }
+};
