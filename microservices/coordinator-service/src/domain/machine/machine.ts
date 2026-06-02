29792eb41ba553d798623a9158ac91c3a87bcb12
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-02 13:24:39 +0200

    feat(coordinator-service): implement MachineId with validation and create package for machine aggregate

--- microservices/coordinator-service/src/domain/machine/machine.ts
+++ microservices/coordinator-service/src/domain/machine/machine.ts
@@ -1,19 +1,24 @@
-import { Emission } from './emission'
+import { Emission } from '../value-objects/emission'
 import { MachineConfig } from './machine-config'
-import { PowerConsumption } from './power-consuption'
-import { Pressure } from './pressure'
-import { Temperature } from './temperature'
-import { Vibration } from './vibration'
+import { PowerConsumption } from '../value-objects/power-consuption'
+import { Pressure } from '../value-objects/pressure'
+import { Temperature } from '../value-objects/temperature'
+import { Vibration } from '../value-objects/vibration'
+import { MachineId } from '../value-objects/machine-id'
 
 export class Machine {
   constructor(
-    public readonly id: string,
+    public readonly id: MachineId,
     public temperature: Temperature,
     public powerConsumption: PowerConsumption,
     public emissions: Emission,
     public vibration: Vibration,
     public pressure: Pressure
   ) {}
+
+  isEqual(other: Machine): boolean {
+    return this.id.equals(other.id)
+  }
 
   isOverheating(config: MachineConfig): boolean {
     return this.temperature.value >= config.temperature.max
