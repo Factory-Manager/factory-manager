dc969a068502c1b945c93dfa2c6957d8b2f02250
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-02 18:38:38 +0200

    test(coordinator-service): add unit test for Range value to validate temperature containment

---
+++ microservices/coordinator-service/test/domain/shared/value-objects/range/range.specs.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest"
+import { Range } from "../../../../../src/domain/shared/value-objects/range/range"
+import { Temperature } from "../../../../../src/domain/machine/value-objects/temperature"
+
+describe("Range", () => {
+
+    const range = new Range(
+        new Temperature(10),
+        new Temperature(100)
+    )
+
+    it("returns true when value is inside range", () => {
+        expect(
+            range.contains(new Temperature(50))
+        ).toBe(true)
+    })
+})
