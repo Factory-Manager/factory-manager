361e25b230c87abbb115f33a64c4d9c4890ca607
david <david.cohen@studio.unibo.it>
2026-06-03 22:21:05 +0200

    feat(logging): add request logger middleware

--- microservices/core-rest-service/src/bootstrap/express.js
+++ microservices/core-rest-service/src/bootstrap/express.js
@@ -3,6 +3,7 @@ import express from 'express'
 
 import { errorHandler } from '../middlewares/error-handler.js'
 import { notFoundHandler } from '../middlewares/not-found-handler.js'
+import { requestLogger } from '../middlewares/request-logger.js'
 import { configureDocs } from './docs.js'
 import { isDatabaseConnected } from './mongoose.js'
 import { createUsersBootstrap } from './users.js'
@@ -16,6 +17,7 @@ export function configureExpress(app) {
   app.use(cors())
   app.use(express.json({ limit: '20mb' }))
   app.use(express.urlencoded({ limit: '20mb', extended: true }))
+  app.use(requestLogger)
 
   app.get('/', (_req, res) => {
     res.json({
