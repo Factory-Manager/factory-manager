684322a2c9377194c90b36eb3deed8c12a513c48
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-07 12:43:40 +0200

    feat(coordinator-service): add TelemetryEvent type and update ProcessTelemetry to handle telemetry input

---
+++ microservices/coordinator-service/src/application/telemetry/dto/telemetry-event.ts
@@ -0,0 +1,9 @@
+export type TelemetryEvent = {
+  machineId: string
+  occurredAt: Date
+  operatingTemperature: number
+  powerConsumption: number
+  emissions: number
+  vibration: number
+  pressure: number
+}
