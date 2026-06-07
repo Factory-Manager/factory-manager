fb480545c1bd3165f480c8934e0ee2bc0b0cd389
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-07 17:50:00 +0200

    refactor(anomaly): remove AnomalyType and threshold from Anomaly class

--- microservices/coordinator-service/src/domain/anomaly/anomaly.ts
+++ microservices/coordinator-service/src/domain/anomaly/anomaly.ts
@@ -1,16 +1,13 @@
 import { MachineId } from '../machine/value-objects/machine-id'
 import { AnomalyEventId } from './value-objects/anomaly-event-id'
-import { AnomalyType } from './value-objects/anomaly-type'
 import { SensorType } from './value-objects/sensor-type'
 
 export class Anomaly {
   constructor(
     public readonly id: AnomalyEventId,
     public readonly machineId: MachineId,
-    public readonly anomalyType: AnomalyType,
     public readonly sensorType: SensorType,
     public readonly value: number,
-    public readonly threshold: number,
     public readonly occurredAt: Date
   ) {}
 }
--- microservices/coordinator-service/src/domain/anomaly/value-objects/anomaly-type.ts
+++
@@ -1,4 +0,0 @@
-export enum AnomalyType {
-  OVER_LIMIT = 'OVER_LIMIT',
-  UNDER_LIMIT = 'UNDER_LIMIT'
-}
