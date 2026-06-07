0d84ae41a99338e4caefdaa57e09ef7bbaaaca13
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-07 17:54:16 +0200

    test(anomaly): add unit tests for temperature policy and anomaly detector

---
+++ microservices/coordinator-service/test/domain/anomaly/services/anomaly-detector.spec.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest"
+import { AnomalyDetector } from "../../../../src/domain/anomaly/services/anomaly-detector"
+import { TemperaturePolicy } from "../../../../src/domain/anomaly/services/policies/temperature-policy"
+import { TelemetryEvent } from "../../../../src/application/telemetry/dto/telemetry-event"
+import { MachineConfig } from "../../../../src/domain/machine/machine-config"
+import { MACHINE_LIMITS } from "../../../constants/machine-limits"
+import { MACHINE_VALUES } from "../../../constants/machine-values"
+import { fakeConfig } from "../../../utils/fake-config"
+
+
+describe('AnomalyDetector', () => {
+    it('should aggregate anomalies from policies', () => {
+        const detector = new AnomalyDetector([
+            new TemperaturePolicy()
+        ])
+
+        const event: TelemetryEvent = {
+            machineId: MACHINE_VALUES.ID,
+            occurredAt: new Date(),
+            operatingTemperature: MACHINE_VALUES.TEMPERATURE.OVER,
+            powerConsumption: MACHINE_VALUES.POWER_CONSUMPTION.SAFE,
+            emissions: MACHINE_VALUES.EMISSION.SAFE,
+            vibration: MACHINE_VALUES.VIBRATION.SAFE,
+            pressure: MACHINE_VALUES.PRESSURE.SAFE
+        };
+        const config: MachineConfig = fakeConfig({
+            temperature: {
+                min: MACHINE_LIMITS.TEMPERATURE.MIN,
+                max: MACHINE_LIMITS.TEMPERATURE.MAX,
+            }
+        });
+        const anomalies = detector.detect(event, config);
+        expect(anomalies).toHaveLength(1);
+        expect(anomalies[0].sensorType).toBe("TEMPERATURE");
+        expect(anomalies[0].value).toBe(event.operatingTemperature);
+    })
+})
