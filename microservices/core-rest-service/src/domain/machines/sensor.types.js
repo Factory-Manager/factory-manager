dd1bc2f52ac78d5a7adfb9ae730fbfada95d23ad
david <david.cohen@studio.unibo.it>
2026-06-16 09:44:06 +0200

    feat(core-rest): add sensor types

---
+++ microservices/core-rest-service/src/domain/machines/sensor.types.js
@@ -0,0 +1,9 @@
+export const SENSOR_TYPES = Object.freeze({
+  POWER_CONSUMPTION: 'powerConsumption',
+  EMISSIONS: 'emissions',
+  OPERATING_TEMPERATURE: 'operatingTemperature',
+  VIBRATION: 'vibration',
+  PRESSURE: 'pressure'
+})
+
+export const SENSOR_TYPE_VALUES = Object.freeze(Object.values(SENSOR_TYPES))
