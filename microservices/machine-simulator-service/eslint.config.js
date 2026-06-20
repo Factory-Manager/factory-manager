0181c9bd3fa51ca6b29166a467b567ef275566c6
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-20 13:16:59 +0200

    feat(machine-simulator-service): enhance machine-simulator-service with linting and formatting scripts

---
+++ microservices/machine-simulator-service/eslint.config.js
@@ -0,0 +1,20 @@
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: ['node_modules', 'dist']
+  },
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module'
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn'
+    }
+  }
+]
