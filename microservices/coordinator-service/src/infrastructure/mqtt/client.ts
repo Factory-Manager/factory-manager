b1cde0a0fab96d85202cc9c7e20c898573cb72be
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-06-27 11:44:48 +0200

    feat(coordinator-service): implement MQTT client creation functionality

---
+++ microservices/coordinator-service/src/infrastructure/mqtt/client.ts
@@ -0,0 +1,24 @@
+import mqtt from 'mqtt'
+import { Logger } from '@/application/ports/logger'
+
+export function createMqttClient(url: string, logger: Logger) {
+  const client = mqtt.connect(url)
+
+  client.on('connect', () => {
+    logger.info('connected')
+  })
+
+  client.on('reconnect', () => {
+    logger.info(`simulator reconnecting...`)
+  })
+
+  client.on('error', (err) => {
+    logger.error('mqtt client error', { err })
+  })
+
+  return {
+    client,
+    close: () =>
+      new Promise<void>((resolve) => client.end(false, {}, () => resolve()))
+  }
+}
