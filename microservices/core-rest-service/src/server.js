21ea404dea4a9b2af92a32948a3baf6e83133a42
david <david.cohen@studio.unibo.it>
2026-07-01 00:44:12 +0200

    chore(core-rest): add graceful shutdown for server and db connection

--- microservices/core-rest-service/src/server.js
+++ microservices/core-rest-service/src/server.js
@@ -2,14 +2,23 @@ import 'dotenv/config'
 
 import { app } from './app.js'
 import { logger } from './bootstrap/logger.js'
-import { connectToDatabase } from './bootstrap/mongoose.js'
+import {
+  connectToDatabase,
+  disconnectFromDatabase
+} from './bootstrap/mongoose.js'
 import { env } from './config/env.js'
 
 async function startServer() {
   await connectToDatabase(env.mongoUri)
 
-  app.listen(env.port, () => {
+  const server = app.listen(env.port, () => {
     logger.info(`core-rest-service listening on port ${env.port}`)
+  })
+
+  process.on('SIGTERM', () => {
+    server.close(() => {
+      disconnectFromDatabase().finally(() => process.exit(0))
+    })
   })
 }
 
