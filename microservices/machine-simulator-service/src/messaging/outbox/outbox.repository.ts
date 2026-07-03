ba7776ee49277808450d264edcc37f45a3ac4d24
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-07-03 17:32:05 +0200

    feat(outbox): add updateStatus to OutboxRepository interface

--- microservices/machine-simulator-service/src/messaging/outbox/outbox.repository.ts
+++ microservices/machine-simulator-service/src/messaging/outbox/outbox.repository.ts
@@ -2,4 +2,5 @@ import { OutboxMessage } from './outbox-message'
 
 export interface OutboxRepository {
   save(message: OutboxMessage): void
+  updateStatus(eventId: string, status: string, attempts: number): void
 }
