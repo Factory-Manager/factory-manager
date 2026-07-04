be18501da9567d659388ebf35a8fe057246dcbac
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-07-04 13:20:08 +0200

    feat(outbox): implement createSqliteDatabase to initialize SQLite database and inbox table

---
+++ microservices/coordinator-service/src/infrastructure/persistence/sqlite/create-sqlite-database.ts
@@ -0,0 +1,20 @@
+import fs from 'node:fs'
+import Database from 'better-sqlite3'
+import path from 'path/win32'
+
+export function createSqliteDatabase(inboxDbPath: string): Database.Database {
+  const dataDir = path.resolve('data')
+  fs.mkdirSync(dataDir, { recursive: true })
+  const dbPath = path.resolve(process.cwd(), inboxDbPath)
+  const db = new Database(dbPath)
+  db.exec(`
+        CREATE TABLE IF NOT EXISTS coordinator_inbox (
+        event_id TEXT PRIMARY KEY,
+        topic TEXT NOT NULL,
+        payload TEXT NOT NULL,
+        status TEXT NOT NULL,
+        received_at TEXT NOT NULL,
+        processed_at TEXT
+    )`)
+  return db
+}
