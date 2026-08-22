1366a783210167b4d2027f53a944294fb2da5ccf
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2026-08-22 12:25:57 +0200

    feat(core-rest-service): add MachineDto interface for machine data representation

---
+++ microservices/coordinator-service/src/infrastructure/adapters/core-rest/dto/machine.dto.ts
@@ -0,0 +1,59 @@
+export interface MachineDto {
+  id: string
+  serial: string
+  name: string
+
+  location: {
+    areaId: string
+  }
+
+  machineState: {
+    currentState: 'operational' | 'off' | 'anomaly'
+    anomalyDetails: unknown[]
+  }
+
+  specifications: {
+    powerConsumption: {
+      measurementUnit: string
+      normalRange: {
+        min: number
+        max: number
+      }
+    }
+
+    emissions: {
+      measurementUnit: string
+      normalRange: {
+        min: number
+        max: number
+      }
+    }
+
+    operatingTemperature: {
+      measurementUnit: string
+      normalRange: {
+        min: number
+        max: number
+      }
+    }
+
+    vibration: {
+      measurementUnit: string
+      normalRange: {
+        min: number
+        max: number
+      }
+    }
+
+    pressure: {
+      measurementUnit: string
+      normalRange: {
+        min: number
+        max: number
+      }
+    }
+  }
+
+  createdAt: string
+  updatedAt: string
+}
